0e93393517314db857fad09e0a5ee521e75df2d9
ArturoGomez <116232430+ArturoGomezGz@users.noreply.github.com>
2024-11-15 23:05:52 +0000

    Debuging

--- Actividades/Colavorativa/5.2/main.cpp
+++ Actividades/Colavorativa/5.2/main.cpp
@@ -2,10 +2,16 @@
 
 int main(){
 
-    HashMap map = HashMap();
+    LinkedList list = LinkedList();
+    list.insert("Jun 20 13:39:21 10.15.174.240:4486 Failed password for illegal user guest");
+    list.insert("Sep 24 17:22:12 10.15.188.234:4145 Failed password for illegal user guest");
+
+    list.print();
+
+    /* HashMap map = HashMap();
     
     map.loadTxt("bitacora3");
-    map.print();
+    map.print(); */
 
     return 0;
 }
